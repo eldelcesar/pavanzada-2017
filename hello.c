0d0dd702d05a095f49b2619c3b2a81064312df2e
Cesar <Cesar@MacBooks-MacBook-Pro.local>
2017-08-10 19:47:24 -0500

    Hello World updated

--- hello.c
+++ hello.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
+/*
+// VERSION 1
 int main (){
   printf("Hello World\n");
+  return 0;
+}*/
+
+// VERSION 2
+int main (int argn, char **argv){
+  for(int i = 1; i < argn; i++){
+    printf("Hello %s\n", argv[i]);
+  }
   return 0;
 }
