3fd816f16d17726c68c72cb1cc3bbfb28c87a787
Cesar <Cesar@MacBooks-MacBook-Pro.local>
2017-08-24 20:45:37 -0500

    Clase 3 – Unit testing. Reestructuracion de carpetas

---
+++ clases/01-calculate_days/calculate_days.h
@@ -0,0 +1,9 @@
+#ifndef __CALCULATE_DAYS__
+#define __CALCULATE_DAYS__
+
+typedef enum month {Jan, Feb, Mar, May, Apr, Jun, Jul, Aug, Sep, Oct, Nov, Dec} Month;
+
+int calculateDays(Month, int, int);
+int isLeapYear(int);
+
+#endif
--- katas/01-length_last_word/length-word.c
+++ katas/01-length_last_word/length-word.c
@@ -2,29 +2,34 @@
 #include <stdio.h>
 #include "length-word.h"
 
-int lengthOfLastWord(char* s) {
+/**************
+ * lengthOfLastWord
+ * Given a string, get the length of the last word
+ * The program will determine when the last word actually starts (from right to left)
+   and then count its length.
+ * @param:
+ *  str: string
+ * @return
+ *  integer with length of last word
+*/
+int lengthOfLastWord(char* str) {
   int i, j, length, count, startCount;
-  // Count will save the size of the last word
-  // Start Count will determine when the last word actually starts (from right to left)
+
   count = 0;
   startCount = 0;
 
-  // Get the lenght of the complete string
-  for(i = 0; s[i] != '\0'; i++){}
+  // Get Length
+  for(i = 0; str[i] != '\0'; i++){}
   length = i;
 
-  // In case the string is not empty
+  // Count the length of last word
   if (length > 0) {
-    // Traverse the char array from back to front
     for (j = (length-1); j >= 0; j--) {
-
-      // When it detects the last letter of the last word, starts the real count
-      if (s[j] != ' ') {
+      if (str[j] != ' ') {
         startCount = 1;
         count++;
       }
-      // In case the real count already started and an empty space is found, it stops
-      if (s[j] == ' ' && startCount == 1) {
+      if (str[j] == ' ' && startCount == 1) {
         return count;
       }
     }
