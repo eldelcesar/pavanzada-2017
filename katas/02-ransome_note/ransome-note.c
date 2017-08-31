11f67f958535abb4a0f1c8997a60c6be684df641
Cesar <Cesar@MacBooks-MacBook-Pro.local>
2017-08-31 17:51:28 -0500

    Ransome Note v 1.0, Genral Case

---
+++ katas/02-ransome_note/ransome-note.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "ransome-note.h"
+#include <stdbool.h>
+
+bool canConstruct(char* ransomNote, char* magazine){
+
+  int count, i, j;
+  count = 0;
+
+  for (i = 0; ransomNote[i] != '\0'; i++) {
+    for (j = 0; magazine[j] != '\0'; j++) {
+      if (ransomNote[i] == magazine[j]) {
+        count++;
+        break;
+      }
+    }
+  }
+
+  if (count == i) {
+    return true;
+  }
+  else{
+    return false;
+  }
+
+}
