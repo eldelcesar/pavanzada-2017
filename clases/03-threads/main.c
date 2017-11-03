a8c072c2f721749af8444d56bb7a5e48ede4a2f3
eldelcesar <cesarjr.espinosa@gmail.com>
2017-11-02 20:15:00 -0600

    Threads - Race Condition

---
+++ clases/03-threads/main.c
@@ -0,0 +1,45 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#define NUM_THREADS 200
+
+unsigned int total;
+
+void *add100(void *threadid){
+  long tid;
+  tid = (long)threadid;
+  unsigned int localTotal;
+  printf("Adding 100 #%ld!\n", tid);
+  localTotal = total;
+  localTotal += 100;
+  sleep(tid%3);
+  total = localTotal;
+  pthread_exit(NULL);
+}
+
+int main(int argc, char *argv[]){
+  pthread_t threads[NUM_THREADS];
+  int rc;
+  long t;
+  total = 0;
+  printf("Initial total = %d \n", total);
+
+  for (t = 0; t < NUM_THREADS; t++){
+    printf("In main: creating thread %ld\n", t);
+    rc = pthread_create(&threads[t], NULL, add100, (void *)t);
+    if (rc)
+    {
+      printf("ERROR; return code from pthread_create() is %d\n", rc);
+      exit(-1);
+    }
+  }
+
+  for (t = 0; t < NUM_THREADS; t++){
+    pthread_join(threads[t], NULL);
+  }
+
+  printf("Final total = %d \n", total);
+  /* Last thing that main() should do */
+  pthread_exit(NULL);
+}
